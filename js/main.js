ed24ed40410f59cc0ad65681ea2b104b4ec9a780
ParkDajeong <sobeast980@gmail.com>
2021-05-22 13:23:14 +0900

    mobile header 구현 완료

--- js/main.js
+++ js/main.js
@@ -6,7 +6,28 @@ $(function() {
     $(".gnb__sub-area").addClass("on"); 
   }, function() {
     $(".gnb__bg").removeClass("on");
-    $(".gnb__sub").stop().slideUp(350);
+    $(".gnb__sub").stop().slideUp(250);
     $(".gnb__sub-area").removeClass("on");
+  });
+
+  // mobile nav
+  $(".btn-menu").on("click", function() {
+    $(this).toggleClass("on");
+    $(".gnb-mo").slideToggle(300);
+    $(".header__inner").toggleClass("on");
+  });
+
+  $(".gnb-mo__main-link").on("click", function() {
+    if(!$(this).parent().hasClass("on")) {
+      $(".gnb-mo__sub").slideUp(300);
+      $(".gnb-mo__main-item").removeClass("on");
+    }
+    $(this).siblings(".gnb-mo__sub").slideToggle(300);
+    $(this).parent().toggleClass("on");
+  });
+
+  $(".gnb-mo__footer-menu-link").on("click", function() {
+    $(this).parent().toggleClass("on");
+    $(this).siblings(".gnb-mo__footer-sub").toggleClass("on");
   });
 });
